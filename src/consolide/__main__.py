1e1173bdadc72c9a1eaa12bd027b7ea0c45e68cd
Leo Araya <larayavrs@gmail.com>
2025-12-19 20:08:30 -0300

    feat(main): nothing important

--- src/consolide/__main__.py
+++ src/consolide/__main__.py
@@ -1,9 +1,4 @@
 from consolide.component import ConsolideComponent
+from consolide.app import ConsolideApp
 
-if __name__ == "__main__":
-    print("Hello world from Consolide!")
-
-    component = ConsolideComponent()
-    component.render()
-    component.update()
-    component.destroy()
+# TODO continue here.
