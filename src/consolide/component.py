f4a584a1dfa4985c2a0e1603a0434edc78d62812
Leo Araya <larayavrs@gmail.com>
2025-12-28 18:41:33 -0300

    feat(component): refine abstract methods and initialize terminal in ConsolideComponent

--- src/consolide/component.py
+++ src/consolide/component.py
@@ -6,16 +6,22 @@ The Component module contains the base class for all components.
 """
 
 from abc import ABC, abstractmethod
-from .exceptions import *
+from consolide.terminal import Terminal
 
 class ConsolideComponent(ABC):
     """This class is the base for all components"""
 
+    def __init__(self, terminal: Terminal) -> None:
+        self.terminal = terminal
+
     @abstractmethod
-    def render(self) -> str: ...
+    def render(self) -> None:
+        pass
     
     @abstractmethod
-    def update(self) -> None: ...
+    def update(self) -> None:
+        pass
     
     @abstractmethod
-    def destroy(self) -> None: ...
+    def destroy(self) -> None:
+        pass
