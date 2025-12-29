a6ec6e92ec1050ccc3df7fe3b3845b0bfd2fa5e3
Leo Araya <larayavrs@gmail.com>
2025-12-28 21:48:51 -0300

    feat(container): add Container widget to group multiple components

---
+++ src/consolide/container.py
@@ -0,0 +1,12 @@
+"""
+Consolide - Container widget
+============================
+
+A container that groups multiple components.
+"""
+
+from typing import Iterable, List
+from consolide.component import ConsolideComponent
+
+class Container(ConsolideComponent):
+    pass
